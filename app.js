c797de7d3db40332f96c15d77411fa10bb20733c
JRachbauer <joshua.rachbauer20@gmail.com>
2020-11-19 08:37:19 +0100

    Complete task 3

---
+++ app.js
@@ -0,0 +1,53 @@
+let clicker = 0;
+
+var passwort = prompt("Gebe dein Passwort ein: ");
+
+while(passwort != "test321") {
+	alert("Passwort falsch!");
+	passwort = prompt("Gebe dein Passwort ein: ");
+}
+
+function Clicker() {
+	
+	clicker = clicker + 1;
+
+	document.getElementById('clicker').innerHTML = clicker;
+}
+
+
+var started, resetTimeoutHandle, resetTimeout = 1000,
+    counter = document.getElementById('cps'),
+    zone = document.getElementById('button'),
+    clicks = 0;
+
+zone.onseclect = zone.onselectstart = function() {
+    return false;
+};
+
+function clicksPerSecond(started, clicks) {
+    return clicks / ((new Date()) - started) * 1000;
+}
+
+function count() {
+    clearTimeout(resetTimeoutHandle);
+    clicks++;
+    counter.innerText = clicksPerSecond(started, clicks);
+    resetTimeoutHandle = setTimeout(reset, resetTimeout);
+    return false;
+}
+
+function start() {
+    started = new Date();
+    clicks = 0;
+    counter.style.opacity = 1;
+    this.onmousedown = count;
+    this.onmousedown();
+    return false;
+}
+
+function reset() {
+    zone.onmousedown = start;
+    counter.style.opacity = 0.3;
+}
+
+reset();
